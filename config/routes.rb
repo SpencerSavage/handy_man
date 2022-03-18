5120dfe72dd160adc13648343e1562a4a89e83c2
Alex Briceno <jalexbriceno@gmail.com>
2022-03-17 20:52:53 -0600

    action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  
+  namespace :api do
+   resources :workers do
+    resources :services
+    end
+    resources :services, except: [:index, :show, :create, :update, :destroy] do
+      resources :comments
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Comment.delete_all
+Service.delete_all
+Worker.delete_all 
+
+5.times do 
+  worker = Worker.create(
+    fname: Faker::TvShows::RuPaul.queen
+    lname: Faker::TvShows::RuPaul.queen
+    phone: '208-867-5309'
+  )
+  3.times do
+    service = Service.create(
+      sname: Faker::TvShows::RuPaul.queen,
+      stype: 'Mechanic'
+      worker_id: worker.id
+    )
+    
+    Comment.create(
+      subject: Faker::Creature::Cat.name,
+      body:Faker::TvShows::RuPaul.queen
+      service_id: service.id
+    )
+  end 
+end
+
+# puts "Number of Billboards: #{Billboard.all.count}"
+# Billboard.all.each do |billboard|  
+#   puts "#{billboard.id}, title: #{billboard.title} desc: #{billboard.description}"
+# end
+
+# puts "Number of Artists: #{Artist.all.count}"
+# Artist.all.each do |artist|  
+#   puts "#{artist.id}, title: #{artist.title} rank: #{artist.rank}"
+# end
+
+# puts "Number of Songs: #{Song.all.count}"
+# Song.all.each do |song|  
+#   puts "#{song.id}, title: #{song.subject} duration: #{song.duration}"
+# end
