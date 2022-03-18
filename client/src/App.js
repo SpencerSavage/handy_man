2a95b21e55d5f485958e029ea2ad9842fddd2f05
Alex Briceno <jalexbriceno@gmail.com>
2022-03-17 20:48:58 -0600

    components

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/shared/Home';
+import Team from './components/shared/Team';
+import Workers from './components/workers/Workers';
+import Services from './components/services/Services';
+import Comments from './components/comments/Comments';
+import Navbar from './components/shared/Navbar';
+import NoMatch from './components/shared/Home';
+
+const App = () => (
+  <>
+  
+  <Navbar />
+  <Routes>
+    <Route path= '/' element={<Home />} />
+    <Route path= '/team' element={<Team />} />
+    <Route path= '/workers' element={<Workers />} />
+    {/* <Route path= '/services' element={<Services />} />
+    <Route path= '/comments' element={<Comments />} /> */}
+    <Route path= '*' element={<NoMatch />} />
+  </Routes>
+</>
+)
+
+export default App;
