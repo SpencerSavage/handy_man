f580f4b0efa14ebc1dc7a2b4ba1a8f338ca87207
Brandon Shay <brandonshayproductions@gmail.com>
2022-03-17 19:20:59 -0600

    comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,48 @@
 class Api::CommentsController < ApplicationController
+  before_action :set_service
+  before_action :set_comment, only: [:update, :show, :destroy]
+
+  def index
+    render json: @service.comments 
+  end
+
+  def show
+    render json: @comment
+  end
+
+  def create
+    @comment = @service.comments.new(comment_params)
+    if comment.save
+       render json: @comment
+    else
+       render json: { errors: @comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comments_params)
+      render json: @comment
+    else
+      render json: { errors: @comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    render json: { message: 'Comment Deleted' }
+  end
+
+  private
+    def set_service
+      @service = Service.find(params[:service_id])
+    end
+
+    def set_comment
+      @comment = @service.comments.find(params[:id])
+    end
+
+    def comments_params
+      params.require(:comment).permit(:subject, :body)
+    end
+
 end
